76a3f7cbde44f57ff28f16d64e789b9959224a3b
33-ssh <mountainxu33@gmail.com>
2024-08-31 23:59:59 +0800

    0831

--- js/script-ctu-inf-itn.js
+++ js/script-ctu-inf-itn.js
@@ -6,6 +6,11 @@ $(function () { /* document ready 一個js檔只要執行一次就可以 */
         $('html,body').animate({ scrollTop: 0 }, 1000);
     });
 
-
+    // 折疊式菜單切換
+    // 折疊式菜單切換
+    $('.menu-icon').click(function () {
+        $('#container-topbar .navigation .menu').toggleClass('active');
+        
+    });
 
 });
